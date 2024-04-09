5cf49368207f1e887a80513728f35066d8af0b98
Younki <younki@Younkis-MacBook-Pro.local>
2024-04-09 02:27:22 -0700

    first commit, fastapi testing

---
+++ main.py
@@ -0,0 +1,32 @@
+from typing import Optional
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
+from random import randrange
+
+app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+my_posts = [{"title": "title of post1","content": "content of post 1", "id": 1},
+            {"title":"favorite food","content":"I like pizza","id":2}]
+
+@app.get("/")
+def root():
+    return {"message": "Welcome !!!"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": my_posts}
+
+
+@app.post("/posts")
+def create_posts(post: Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0,1000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}

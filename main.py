598e258739ea9b292e019de9a719e350052bc762
Younki <younki@Younkis-MacBook-Pro.local>
2024-04-15 05:55:44 -0700

    delete functionality added

--- main.py
+++ main.py
@@ -19,7 +19,14 @@ def find_post(id):
     for p in my_posts:
         if p["id"] == id:
             return p
+
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
         
+        
+
 
 
 @app.get("/")
@@ -31,7 +38,7 @@ def get_posts():
     return {"data": my_posts}
 
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
     post_dict = post.dict()
     post_dict['id'] = randrange(0,1000000)
@@ -52,5 +59,14 @@ def get_post(id: int, response: Response):
     return {"post_detail":post}
 
 
-
+@app.delete("/posts/{id}",status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id:int):
+    # deleting post
+    # find the index in the array that has required ID
+    # my_post.pop(index)
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post with id: {id} does not exist")
+    my_posts.pop(index)
+    return Response(status_code = status.HTTP_204_NO_CONTENT)
 

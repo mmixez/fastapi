8cbb08b9b484da4e15512453ce56b3f6e157e69f
Younki <younki@Younkis-MacBook-Pro.local>
2024-04-15 05:09:20 -0700

    HTTP Exception added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import Body, FastAPI, Response, status
+from fastapi import Body, FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
 from random import randrange
 
@@ -40,14 +40,15 @@ def create_posts(post: Post):
 
 
 
-
-
 @app.get("/posts/{id}")
 def get_post(id: int, response: Response):
     post = find_post(id)
     if not post:
-        response.status_code = status.HTTP_404_NOT_FOUND
-
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail = f"post with id: {id} was not found")
+                            
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {'message': f"post with id: {id} was not found"}
     return {"post_detail":post}
 
 
